fefe841df199eb6852149ab1caa0fa6bbc653200
LucasRBeckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-03-14 10:35:45 -0300

    Update built_in.py

--- built_in.py
+++ built_in.py
@@ -1,6 +1,6 @@
 """
 https://docs.python.org/pt-br/3/library/stdtypes.html
-Imutáveis que vimos: str, int, float, bool
+Imutáveis: str, int, float, bool
 """
 string = '1000'
 # outra_variavel = f'{string[:3]}ABC{string[4:]}'
